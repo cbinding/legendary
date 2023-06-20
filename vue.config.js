5b3ae357407d25b441092730012aa4fcd19f6f41
Ceri Binding <ceri.binding@southwales.ac.uk>
2023-06-20 11:15:31 +0000

    rebuilt and redeployed

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/prisma/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/legendary/" : "/",
   transpileDependencies: true,
   devServer: {
     allowedHosts: 'all',
